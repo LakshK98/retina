9c47a5a59e3a7bdfff5f3cba20fcbe562b58e662
Laksh Kotian <lakshkotian98@gmail.com>
2025-09-03 09:18:59 -0700

    pktmon parsing skeleton

--- pkg/plugin/ebpfwindows/datapath_drop_windows.go
+++ pkg/plugin/ebpfwindows/datapath_drop_windows.go
@@ -50,6 +50,64 @@ type DropNotify struct {
 	Ifindex  uint32
 }
 
+type PktmonDropNotify struct {
+	Type     uint8
+	SubType  uint8
+	Source   uint16
+	Hash     uint32
+	OrigLen  uint32
+	CapLen   uint16
+	Version  uint16
+	SrcLabel identity.NumericIdentity
+	DstLabel identity.NumericIdentity
+	DstID    uint32
+	Line     uint16
+	File     uint8
+	ExtError int8
+	Ifindex  uint32
+}
+
+// DecodeDropNotify will decode 'data' into the provided DropNotify structure
+func DecodePktmonDrop(data []byte, dn *DropNotify) error {
+	pdn := &PktmonDropNotify{}
+	if err := pdn.decodePktmonDrop(data); err != nil {
+		return err
+	}
+	*dn = DropNotify(*pdn)
+	return nil
+}
+
+func (n *PktmonDropNotify) decodePktmonDrop(data []byte) error {
+	if l := len(data); l < dropNotifyV1Len {
+		return fmt.Errorf("%w: expected at least %d but got %d", errUnexpectedDropNotifyLength, dropNotifyV1Len, l)
+	}
+
+	version := byteorder.Native.Uint16(data[14:16])
+
+	// Check against max version.
+	if version > DropNotifyVersion1 {
+		return fmt.Errorf("%w: Unrecognized drop event version %d", errInvalidDropNotifyVersion, version)
+	}
+
+	// Decode logic for version >= v0/v1.
+	n.Type = data[0]
+	n.SubType = data[1]
+	n.Source = byteorder.Native.Uint16(data[2:4])
+	n.Hash = byteorder.Native.Uint32(data[4:8])
+	n.OrigLen = byteorder.Native.Uint32(data[8:12])
+	n.CapLen = byteorder.Native.Uint16(data[12:14])
+	n.Version = version
+	n.SrcLabel = identity.NumericIdentity(byteorder.Native.Uint32(data[16:20]))
+	n.DstLabel = identity.NumericIdentity(byteorder.Native.Uint32(data[20:24]))
+	n.DstID = byteorder.Native.Uint32(data[24:28])
+	n.Line = byteorder.Native.Uint16(data[28:30])
+	n.File = data[30]
+	n.ExtError = int8(data[31])
+	n.Ifindex = byteorder.Native.Uint32(data[32:36])
+
+	return nil
+}
+
 // DecodeDropNotify will decode 'data' into the provided DropNotify structure
 func DecodeDropNotify(data []byte, dn *DropNotify) error {
 	return dn.decodeDropNotify(data)
