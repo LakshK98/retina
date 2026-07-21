6ef356cd97c371c5bdda3aae18d1e23dc896661c
Vinod K L Swamy <vinodko@microsoft.com>
2026-07-21 13:53:58 -0700

    Removed usage of the hubble parser options

--- pkg/capture/file/timestamp.go
+++ pkg/capture/file/timestamp.go
@@ -27,8 +27,3 @@ func StringToTimestamp(timestamp string) (*Timestamp, error) {
 	}
 	return &Timestamp{parsedTime}, nil
 }
-
-// TimeToString converts a Timestamp to a string in the capture file name format.
-func TimeToString(t Timestamp) string {
-	return t.Time.UTC().Format(captureFileNameTimestampFormat)
-}
